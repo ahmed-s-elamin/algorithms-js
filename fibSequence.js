2beb4e7f934a2886665c5746f8058bc18b174b6d
ahmed elamin <ahmedselamin@gmail.com>
2023-01-13 18:41:33 +0200

    done

---
+++ fibSequence.js
@@ -0,0 +1,14 @@
+function fib(n) {
+  const fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+
+console.log(fib(2));
+console.log(fib(3));
+console.log(fib(4));
+console.log(fib(5));
+console.log(fib(6));
+console.log(fib(7));
