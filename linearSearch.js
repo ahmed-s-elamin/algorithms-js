7edd449e5f4b9a71e58c1a95a3361efb3621c8a0
ahmed elamin <ahmedselamin@gmail.com>
2023-01-14 20:17:50 +0200

    done

---
+++ linearSearch.js
@@ -0,0 +1,12 @@
+// Given an array of 'n' elements and a target 't'
+// find the index of 't' in the array
+// return -1 if not found
+
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
+  }
+  return -1;
+}
+
+console.log(linearSearch([-5, 2, 10, 4, 6], 10)); //2
