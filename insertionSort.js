5f85b67293a2941385bf08efe368afec6c1b54a6
ahmed elamin <ahmedselamin@gmail.com>
2023-01-15 10:31:57 +0200

    done

--- insertionSort.js
+++ insertionSort.js
@@ -1,7 +1,7 @@
 //given an array of integers, sort array
 
 function insertSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     let nti = arr[i];
     let j = i - 1;
     while (j >= 0 && arr[j] > nti) {
