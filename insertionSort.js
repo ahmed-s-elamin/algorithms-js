19c1080dd1cfb197692427328f76d5ab6abf7d2b
ahmed elamin <ahmedselamin@gmail.com>
2023-01-17 09:26:29 +0200

    done

--- insertionSort.js
+++ insertionSort.js
@@ -1,11 +1,13 @@
 //given an array of integers, sort array
 
 function insertSort(arr) {
+  //since index 0 is sorted traverse 1 to length-1
   for (let i = 1; i < arr.length; i++) {
-    let nti = arr[i];
-    let j = i - 1;
+    let nti = arr[i]; //number to insert
+    let j = i - 1; //sorted part
+    //iterating over the sorted part
     while (j >= 0 && arr[j] > nti) {
-      arr[j + 1] = arr[j];
+      arr[j + 1] = arr[j]; //shifting sorted element to right
       j--;
     }
     arr[j + 1] = nti;
