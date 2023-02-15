3643e00b0877c490d9073081f936bdd222d3c6c4
ahmed elamin <ahmedselamin@gmail.com>
2023-02-15 22:46:41 +0200

    done

--- mergeSort.js
+++ mergeSort.js
@@ -6,11 +6,11 @@ divide arr into subarr that contain one element
 recursively merge the subarr 
 */
 
-function quickSort(arr) {
-  //base for recursion
+function mergeSort(arr) {
   if (arr.length < 2) {
     return arr;
   }
+
   const mid = Math.floor(arr.length / 2);
   const left = arr.slice(0, mid);
   const right = arr.slice(mid);
@@ -20,18 +20,17 @@ function quickSort(arr) {
 function merge(left, right) {
   const sorted = [];
   while (left.length && right.length) {
-    //compare the first element in both arr and push the smaller into sorted
     if (left[0] <= right[0]) {
       sorted.push(left.shift());
     } else {
       sorted.push(right.shift());
     }
   }
-  return [...sorted, ...left, ...right]; //merging
-}
 
+  return [...sorted, ...left, ...right];
+}
 //O(nlogn)
 
-const arr = [2, 5, 3, 4, 1];
-quickSort(arr);
-console.log(arr);
+const arr = [8, 20, -2, 4, -6];
+
+console.log(mergeSort(arr));
