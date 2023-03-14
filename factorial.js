1d8ae2de79ad579e046c2fb8f389e7dbb44e5126
eddyiiz <eddyxsayf@gmail.com>
2023-03-14 08:48:25 +0200

    .

--- factorial.js
+++ factorial.js
@@ -1,10 +1,12 @@
 function factorial(n) {
   let res = 1;
   for (let i = 2; i <= n; i++) {
-    res = res * i;
+    res *= i;
   }
   return res;
 }
+
+//o(n)
 
 console.log(factorial(0)); //1
 console.log(factorial(5)); //120
