dbc95c75fd3006eb2b125ca9ff4e82f3cb35dd1c
eddyiiz <eddyxsayf@gmail.com>
2023-03-14 08:43:07 +0200

    .

--- fibSequence.js
+++ fibSequence.js
@@ -1,9 +1,15 @@
-function fib(n) {
+function fibonacci(n) {
+  //first 2 numbers always 0, 1
   const fib = [0, 1];
+  //populating array with sequence
   for (let i = 2; i < n; i++) {
     fib[i] = fib[i - 1] + fib[i - 2];
   }
   return fib;
 }
 
-console.log(fib(6));
+//o(n)
+
+console.log(fibonacci(1)); //[0, 1]
+console.log(fibonacci(2)); //[0, 1]
+console.log(fibonacci(3)); //[0, 1, 1]
