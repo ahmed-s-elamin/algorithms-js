d80e7f3f685c7c769c3b35511c2276b06fb3c006
eddyiiz <eddyxsayf@gmail.com>
2023-03-15 19:30:54 +0200

    done

--- climbing.js
+++ climbing.js
@@ -2,16 +2,25 @@
 //count the number of distinct ways to climb to top
 //you can either climb 1 step or 2 steps at a time
 
-function climbing(n) {
-  const ways = [1, 2]; //allowed climbs
-  for (let i = 2; i <= n; i++) {
-    ways[i] = ways[i - 1] + ways[i - 2];
-  }
-  return ways[n - 1];
+// function climbing(n) {
+//   const ways = [1, 2]; //allowed climbs
+//   for (let i = 2; i <= n; i++) {
+//     ways[i] = ways[i - 1] + ways[i - 2];
+//   }
+//   return ways[n - 1];
+// }
+
+//recursive
+function climb(n) {
+  if (n < 2) return 1;
+
+  return climb(n - 1) + climb(n - 2);
 }
 
-console.log(climbing(1));
-console.log(climbing(2));
-console.log(climbing(3));
-console.log(climbing(4));
-console.log(climbing(5));
+//O(2^n)
+
+console.log(climb(1));
+console.log(climb(2));
+console.log(climb(3));
+console.log(climb(4));
+console.log(climb(5));
